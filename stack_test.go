332d978bd0c8593cc0bcabbd05ec94d6d76db10b
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2020-02-12 19:39:33 +0900

    add test for Stack

---
+++ stack_test.go
@@ -0,0 +1,54 @@
+package stack
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPush(t *testing.T) {
+	s := Stack{}
+	s.Push("foo")
+	s.Push("bar")
+
+	want := Stack([]string{"foo", "bar"})
+	if !reflect.DeepEqual(s, want) {
+		// TODO: more detail message
+		t.Error("error")
+		return
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := Stack([]string{"foo", "bar"})
+	t.Run("pop 1", func(t *testing.T) {
+		got := s.Pop()
+		wantVal := "bar"
+		if got != wantVal {
+			// TODO: more detail message
+			t.Error("error on 1 val")
+			return
+		}
+		want := Stack([]string{"foo"})
+		if !reflect.DeepEqual(s, want) {
+			// TODO: more detail message
+			t.Error("error on 1")
+			return
+		}
+	})
+
+	t.Run("pop 2", func(t *testing.T) {
+		got := s.Pop()
+		wantVal := "foo"
+		if got != wantVal {
+			// TODO: more detail message
+			t.Error("error on 2 val")
+			return
+		}
+		want := Stack{}
+		if !reflect.DeepEqual(s, want) {
+			// TODO: more detail message
+			t.Error("error on 2")
+			return
+		}
+	})
+}
